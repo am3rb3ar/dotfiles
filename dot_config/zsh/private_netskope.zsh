4845e6aa42015e6f225e9017937c8ba126270b90
Daniel Blum <daniel.blum@analog.com>
2026-04-23 18:24:47 -0400

    chaging how netskope variables are added

---
+++ dot_config/zsh/private_netskope.zsh
@@ -0,0 +1,11 @@
+################################################################################
+# NETSKOPE CERTIFICATE CONFIGURATION
+
+export AWS_CA_BUNDLE="/tmp/netskope-test/nscacert_combined.pem"
+export REQUESTS_CA_BUNDLE="/tmp/netskope-test/nscacert_combined.pem"
+export SSL_CERT_FILE="/tmp/netskope-test/nscacert_combined.pem"
+export NODE_EXTRA_CA_CERTS="/tmp/netskope-test/nscacert_combined.pem"
+export GIT_SSL_CAINFO="/tmp/netskope-test/nscacert_combined.pem"
+
+# END NETSKOPE CERTIFICATE CONFIGURATION
+################################################################################
