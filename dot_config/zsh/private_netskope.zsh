1edde5126a44282485fe4e3b8ee6a5188b9755fd
Daniel Blum <daniel.blum@analog.com>
2026-04-23 20:40:04 -0400

    fixing netskope config

--- dot_config/zsh/private_netskope.zsh
+++ dot_config/zsh/private_netskope.zsh
@@ -1,11 +1,11 @@
 ################################################################################
 # NETSKOPE CERTIFICATE CONFIGURATION
 
-export AWS_CA_BUNDLE="/tmp/netskope-test/nscacert_combined.pem"
-export REQUESTS_CA_BUNDLE="/tmp/netskope-test/nscacert_combined.pem"
-export SSL_CERT_FILE="/tmp/netskope-test/nscacert_combined.pem"
-export NODE_EXTRA_CA_CERTS="/tmp/netskope-test/nscacert_combined.pem"
-export GIT_SSL_CAINFO="/tmp/netskope-test/nscacert_combined.pem"
+export AWS_CA_BUNDLE="/Users/Shared/Netskope/nscacert_combined.pem"
+export REQUESTS_CA_BUNDLE="/Users/Shared/Netskope/nscacert_combined.pem"
+export SSL_CERT_FILE="/Users/Shared/Netskope/nscacert_combined.pem"
+export NODE_EXTRA_CA_CERTS="/Users/Shared/Netskope/nscacert_combined.pem"
+export GIT_SSL_CAINFO="/Users/Shared/Netskope/nscacert_combined.pem"
 
 # END NETSKOPE CERTIFICATE CONFIGURATION
 ################################################################################
