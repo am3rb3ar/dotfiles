6d1c310fa35c016ec5c44c971f19998cc2150964
Daniel Blum <daniel.blum@analog.com>
2026-04-25 18:09:01 -0400

    seeing what I can do about this netskope file

---
+++ dot_config/fish/conf.d/private_netskope.fish
@@ -0,0 +1,13 @@
+
+################################################################################
+# NETSKOPE CERTIFICATE CONFIGURATION
+
+set -x AWS_CA_BUNDLE "/Users/Shared/Netskope/nscacert_combined.pem"
+set -x REQUESTS_CA_BUNDLE "/Users/Shared/Netskope/nscacert_combined.pem"
+set -x SSL_CERT_FILE "/Users/Shared/Netskope/nscacert_combined.pem"
+set -x NODE_EXTRA_CA_CERTS "/Users/Shared/Netskope/nscacert_combined.pem"
+set -x GIT_SSL_CAINFO "/Users/Shared/Netskope/nscacert_combined.pem"
+
+# END NETSKOPE CERTIFICATE CONFIGURATION
+################################################################################
+
