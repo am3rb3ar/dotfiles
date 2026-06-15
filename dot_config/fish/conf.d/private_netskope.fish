8e549c1f7a53729c4305379f9bc4a2a0cadcd095
Daniel Blum <am3rb3ar@proton.me>
2026-06-15 12:58:18 -0400

    updating netskope for cargo

--- dot_config/fish/conf.d/private_netskope.fish
+++ dot_config/fish/conf.d/private_netskope.fish
@@ -10,6 +10,7 @@ set -x SSL_CERT_FILE $NETSKOPE_PATH
 set -x NODE_EXTRA_CA_CERTS $NETSKOPE_PATH
 set -x GIT_SSL_CAINFO $NETSKOPE_PATH
 set -x DENO_CERT $NETSKOPE_PATH
+set -x CARGO_HTTP_CAINFO $NETSKOPE_PATH
 
 # END NETSKOPE CERTIFICATE CONFIGURATION
 ################################################################################
