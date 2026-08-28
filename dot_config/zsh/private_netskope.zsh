315293b8fcd68830194409d9b05d2a0fe01e588a
Daniel Blum <am3rb3ar@proton.me>
2026-08-28 15:40:50 -0400

    making netskope stuff easier

--- dot_config/zsh/private_netskope.zsh
+++ dot_config/zsh/private_netskope.zsh
@@ -1,13 +1,14 @@
 ################################################################################
 # NETSKOPE CERTIFICATE CONFIGURATION
 
-export AWS_CA_BUNDLE="/Users/Shared/Netskope/nscacert_combined.pem"
-export REQUESTS_CA_BUNDLE="/Users/Shared/Netskope/nscacert_combined.pem"
-export SSL_CERT_FILE="/Users/Shared/Netskope/nscacert_combined.pem"
-export NODE_EXTRA_CA_CERTS="/Users/Shared/Netskope/nscacert_combined.pem"
-export GIT_SSL_CAINFO="/Users/Shared/Netskope/nscacert_combined.pem"
-export CARGO_HTTP_CAINFO="/Users/Shared/Netskope/nscacert_combined.pem"
-export NIX_SSL_CERT_FILE="/Users/Shared/Netskope/nscacert_combined.pem"
+export NETSKOPE_PATH="/Users/Shared/Netskope/nscacert_combined.pem"
+export AWS_CA_BUNDLE="$NETSKOPE_PATH"
+export REQUESTS_CA_BUNDLE="$NETSKOPE_PATH"
+export SSL_CERT_FILE="$NETSKOPE_PATH"
+export NODE_EXTRA_CA_CERTS="$NETSKOPE_PATH"
+export GIT_SSL_CAINFO="$NETSKOPE_PATH"
+export CARGO_HTTP_CAINFO="$NETSKOPE_PATH"
+export NIX_SSL_CERT_FILE="$NETSKOPE_PATH"
 
 # END NETSKOPE CERTIFICATE CONFIGURATION
 ################################################################################
