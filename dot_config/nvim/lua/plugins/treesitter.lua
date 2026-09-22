77cc6cf068ed9c55f578045acd8932c054009446
Daniel Blum <am3rb3ar@proton.me>
2026-09-22 09:22:25 -0400

    adding treesitter stuff for d2

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -99,4 +99,10 @@ return {
 			})
     end
 	},
+  {
+    "ravsii/tree-sitter-d2",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    version = "*", -- use the latest git tag instead of main
+    build = "make nvim-install",
+  },
 }
