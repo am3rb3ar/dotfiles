bd07dac7da1d500671c4f918639fcc7131d5eb65
am3rb3ar <am3rb3ar@proton.me>
2026-09-26 10:54:20 -0400

    adding neoscroll plugin

---
+++ dot_config/nvim/lua/plugins/neoscroll.lua
@@ -0,0 +1,8 @@
+return {
+  "karb94/neoscroll.nvim",
+  opts = {
+    -- easing options: linear, quadratic, cubic, quartic, quintic, circular, sine
+    easing = "sine",
+    duration_multiplier = 0.7,
+  },
+}
